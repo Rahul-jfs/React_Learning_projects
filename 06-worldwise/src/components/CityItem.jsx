98ecb43142409edd8cda420a1028e0e9e3313ef9
Rahul-jfs <ra1290hul@gmail.com>
2025-08-31 23:20:11 +0530

    added the routes to the form and the map

--- 06-worldwise/src/components/CityItem.jsx
+++ 06-worldwise/src/components/CityItem.jsx
@@ -1,4 +1,5 @@
-import styles from "./CityItem.module.css"
+import { Link } from "react-router-dom";
+import styles from "./CityItem.module.css";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -8,15 +9,22 @@ const formatDate = (date) =>
     weekday: "long",
   }).format(new Date(date));
 
-const CityItem = ({city}) => {
-    const {cityName, emoji, date } = city;
-    
-    return <li className={styles.cityItem}>
+const CityItem = ({ city }) => {
+  const { cityName, emoji, date, id, position } = city;
+
+  return (
+    <li>
+      <Link
+        className={styles.cityItem}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
-        <time className={styles.date} >{formatDate(date)}</time>
-        <button className={styles.deleteBtn} > &times;</button>
+        <time className={styles.date}>{formatDate(date)}</time>
+        <button className={styles.deleteBtn}> &times;</button>
+      </Link>
     </li>
-}
+  );
+};
 
-export default CityItem
+export default CityItem;
