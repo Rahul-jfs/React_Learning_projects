c39f8a44556063f15a15fbbaedde7925e852bc19
Rahul-jfs <ra1290hul@gmail.com>
2025-09-22 21:10:35 +0530

    styling with tailwindcss

--- 10-Fast-react-pizza/src/features/order/SearchOrder.jsx
+++ 10-Fast-react-pizza/src/features/order/SearchOrder.jsx
@@ -20,6 +20,7 @@ export default function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="w-28 rounded-full px-4 py-2 bg-yellow-100 placeholder:text-stone-400 sm:focus:w-72 sm:w-64 transition-all focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-offset-2 "
       />
     </form>
   );
--- 10-Fast-react-pizza/src/ui/Header.jsx
+++ 10-Fast-react-pizza/src/ui/Header.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 export default function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="flex justify-between items-center bg-yellow-400 uppercase px-4 py-3 border-b border-stone-200 sm:px-6 font-Roboto">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
 
       <SearchOrder />
-      <p>Jonas</p>
+      <Username />
     </header>
   );
 }
