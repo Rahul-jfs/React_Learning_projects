9edaafb92ea44f232024529b2064a471e73cd2b8
Rahul-jfs <ra1290hul@gmail.com>
2025-10-24 14:35:50 +0530

    booking pagination

--- 11-The-wild-Oasis/src/features/bookings/useBookings.jsx
+++ 11-The-wild-Oasis/src/features/bookings/useBookings.jsx
@@ -1,11 +1,51 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
-  const { isLoading, data: bookings } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+  const queryClient = useQueryClient();
+  const [searchParams] = useSearchParams();
+
+  const filterValue = searchParams.get("status");
+
+  //FILTER
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // { field: "totalPrice", value: 5000, method: "gte" };
+
+  //SORT
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
+  //PAGINATION
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
+  //QUERY
+  const { isLoading, data: { data: bookings, count } = {} } = useQuery({
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
-  return { isLoading, bookings };
+  // const bookings = data?.data ?? [];
+  // const count = bookings.length;
+
+  //PRE-FETCHING
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
+  return { isLoading, bookings, count };
 }
