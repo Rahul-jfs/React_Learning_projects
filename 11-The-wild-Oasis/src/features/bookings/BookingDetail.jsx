3599632dd8a28ff32f5436d9b762cff44e3dd547
Rahul-jfs <ra1290hul@gmail.com>
2025-10-27 21:23:49 +0530

    delete booking

--- 11-The-wild-Oasis/src/features/bookings/BookingDetail.jsx
+++ 11-The-wild-Oasis/src/features/bookings/BookingDetail.jsx
@@ -12,6 +12,10 @@ import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./useBooking";
 import Spinner from "../../ui/Spinner";
 import { useNavigate } from "react-router-dom";
+import { useCheckout } from "../check-in-out/useCheckout";
+import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
+import { useDeleteBooking } from "./useDeleteBooking";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -21,6 +25,8 @@ const HeadingGroup = styled.div`
 
 function BookingDetail() {
   const { booking, isLoading } = useBooking();
+  const { checkout, isCheckingOut } = useCheckout();
+  const { deleteBooking, isDeleting } = useDeleteBooking();
 
   const moveBack = useMoveBack();
   const navigate = useNavigate();
@@ -53,6 +59,25 @@ function BookingDetail() {
             Check In
           </Button>
         )}
+
+        {status === "checked-in" && (
+          <Button onClick={() => checkout(bookingId)}>Check Out</Button>
+        )}
+
+        <Modal>
+          <Modal.Open opens="delete">
+            <Button variation="danger">Delete booking</Button>
+          </Modal.Open>
+          <Modal.Window name="delete">
+            <ConfirmDelete
+              resourceName="booking"
+              disabled={isDeleting}
+              onConfirm={() =>
+                deleteBooking(bookingId, { onSettled: () => navigate(-1) })
+              }
+            />
+          </Modal.Window>
+        </Modal>
 
         <Button variation="secondary" onClick={moveBack}>
           Back
---
+++ 11-The-wild-Oasis/src/features/bookings/useDeleteBooking.js
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteBooking as deleteBookingapi } from "../../services/apiBookings";
+import toast from "react-hot-toast";
+
+export function useDeleteBooking() {
+  const queryClient = useQueryClient();
+
+  const { mutate: deleteBooking, isLoading: isDeleting } = useMutation({
+    mutationFn: deleteBookingapi,
+    onSuccess: () => {
+      toast.success("Booking deleted successfully");
+      queryClient.invalidateQueries({ queryKey: ["bookings"] });
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+
+  return { deleteBooking, isDeleting };
+}
