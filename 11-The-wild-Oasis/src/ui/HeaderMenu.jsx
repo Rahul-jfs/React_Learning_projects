9dbb9eb005b3c5d70b8fd74d1bb85fc6e9b112d0
Rahul-jfs <ra1290hul@gmail.com>
2025-11-01 12:47:09 +0530

    Final updates

--- 11-The-wild-Oasis/src/ui/HeaderMenu.jsx
+++ 11-The-wild-Oasis/src/ui/HeaderMenu.jsx
@@ -16,7 +16,7 @@ function HeaderMenu() {
   return (
     <StyledHeaderMenu>
       <li>
-        <ButtonIcon onClick={() => navigate("")}>
+        <ButtonIcon onClick={() => navigate("/account")}>
           <HiOutlineUser />
         </ButtonIcon>
       </li>
